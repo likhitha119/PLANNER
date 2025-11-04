23e5180c1c2adb2dd347f01f2973faba21dc2e8a
likhitha119 <likhithatalagapu7@gmail.com>
2025-11-04 10:30:13 +0530

    chore: use import.meta.glob query '?url' to remove deprecation warning

--- frontend/src/utils/localImages.js
+++ frontend/src/utils/localImages.js
@@ -2,7 +2,8 @@ export const getLocalImages = () => {
   // Import all images from src/assets/photos as URLs at build time
   const modules = import.meta.glob('../assets/photos/*.{jpg,jpeg,png,webp}', {
     eager: true,
-    as: 'url'
+    query: '?url',
+    import: 'default'
   })
   return Object.values(modules)
 }
